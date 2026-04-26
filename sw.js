4a9fab65612bd8b9151d1f2df99a722f92baac47
chimenchen <chimenchen@gmail.com>
2026-04-26 16:32:53 +0800

    系聯：修正中古音解析問題，少量重構。

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1.0.536';
+const CACHE_VERSION = '1.0.538';
 const CACHE_NAME = `koukoushengsheng-${CACHE_VERSION}`;
 
 const urlsToCache = [
--- xilian-web.js
+++ xilian-web.js
@@ -775,11 +775,11 @@ function __wbg_get_imports() {
         const ret = makeMutClosure(arg0, arg1, 16, __wbg_adapter_18);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1214 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper1212 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 16, __wbg_adapter_18);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper2547 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper2549 = function(arg0, arg1, arg2) {
         const ret = makeClosure(arg0, arg1, 16, __wbg_adapter_25);
         return addHeapObject(ret);
     };
